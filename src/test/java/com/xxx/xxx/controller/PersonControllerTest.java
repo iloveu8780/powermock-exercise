3d50183123a4c69777a511c804514bb0c9704725
iloveu8780 <ilove8780@163.com>
2019-07-24 23:25:47 +0800

    test: add tests for PersonController and PersonService

---
+++ src/test/java/com/xxx/xxx/controller/PersonControllerTest.java
@@ -0,0 +1,38 @@
+package com.xxx.xxx.controller;
+
+import com.xxx.xxx.domain.Person;
+import com.xxx.xxx.service.PersonService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.BDDMockito.given;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class PersonControllerTest {
+
+    @InjectMocks
+    private PersonController personController;
+
+    @Mock
+    private PersonService personService;
+
+    @Test
+    public void should_get_name() {
+        // given
+        String name = "lkh";
+        Person person = new Person("lkh", "lkh", new BigDecimal(100));
+        given(personService.find((argThat(argument -> argument.getName().equals(name))))).willReturn(person);
+        // when
+        String actualName = personController.getName(name);
+        // then
+        assertThat(actualName).isEqualTo(person.getFirstName() + "," + person.getLastName());
+    }
+}
---
+++ src/test/java/com/xxx/xxx/service/PersonServiceTest.java
@@ -0,0 +1,59 @@
+package com.xxx.xxx.service;
+
+import com.xxx.xxx.domain.Person;
+import com.xxx.xxx.domain.PersonRequest;
+import com.xxx.xxx.utils.TimeUnit;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
+import static org.powermock.api.support.membermodification.MemberModifier.suppress;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TimeUnit.class)
+@SuppressStaticInitializationFor("com.xxx.xxx.utils.SalaryCalculator")
+public class PersonServiceTest {
+
+    private PersonService personService;
+
+    @Before
+    public void setUp() {
+        personService = new PersonService();
+    }
+
+    @Test
+    public void should_find_valid_person() {
+        // given
+        PersonRequest personRequest = new PersonRequest("James");
+        suppress(method(TimeUnit.class, "sleep"));
+
+        // when
+        Person actualPerson = personService.find(personRequest);
+
+        // then
+        assertThat(actualPerson.getFirstName()).isEqualTo("Merson");
+        assertThat(actualPerson.getLastName()).isEqualTo("James");
+        assertThat(actualPerson.getSalary()).isEqualTo(new BigDecimal(20));
+    }
+
+    @Test
+    public void should_find_none_person() {
+        // given
+        PersonRequest personRequest = new PersonRequest("");
+
+        // when
+        Person actualPerson = personService.find(personRequest);
+
+        // then
+        assertThat(actualPerson.getFirstName()).isEqualTo("None");
+        assertThat(actualPerson.getLastName()).isEqualTo("None");
+        assertThat(actualPerson.getSalary()).isEqualTo(BigDecimal.ZERO);
+    }
+}
